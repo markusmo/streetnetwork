9b35a0fe3a22b746e96d122310223da8bfd5f11b
markusmo <markus.mo@gmx.net>
2014-11-18 23:13:08 +0100

    Inital netbeans project commit

---
+++ src/streetnetwork/StreetNetwork.java
@@ -0,0 +1,16 @@
+package streetnetwork;
+
+/**
+ *
+ * @author Markus Mohanty <markus.mo at gmx.net>
+ */
+public class StreetNetwork {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
